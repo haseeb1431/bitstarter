aa3fd6c8f2d31314c349113a29f8689247eb2919
Muhammad Haseeb Asif <haseeb1431@gmail.com>
2013-07-13 12:57:37 +0500

    index html with custom msg

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
 var express = require('express');
+var fs=		require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 23!');
+  
+  var file=new Buffer(100);
+  file=fs.readFileSync("index.html");
+  //console.log("here");
+  response.send(file.toString());
+  
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  var file=new Buffer(100);
+  file=fs.readFileSync("index.html");
+  console.log(file.toString());
 });
